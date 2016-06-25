b93f80ab85100cff82e22f2efb034b40e3ff2a01
ShockwaVee <davor.tvoric@gmail.com>
2016-06-25 22:14:34 +0200

    Dodani bounce efekti na elemente forme

--- js/script.js
+++ js/script.js
@@ -91,3 +91,28 @@
 
     $window.on('scroll resize', check_if_in_view);
     $window.trigger('scroll');
+
+    var $animation_elements_once = $('.animation-once');
+
+    function check_if_in_view_once() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
+
+        $.each($animation_elements_once, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('in-view');
+
+            }
+        });
+    }
+
+    $window.on('scroll resize', check_if_in_view_once);
+    $window.trigger('scroll');
